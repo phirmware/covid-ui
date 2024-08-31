5e98d031080225dfc64a3d46977a9d68a56bde73
Chibuzor Ifeanyi Ojukwu <chibuzor.ojukwu@gmail.com>
2024-08-31 20:20:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const morgan = require('morgan');
 const covidFacts = require('covid-facts');
 const url = require('url');
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
 const app = express();
 
 // app.use(logger());
@@ -24,35 +22,6 @@ app.get('/random', (req, res) => {
         statusCode: 200,
         fact,
     });
-});
-
-app.use('/proxy', (req, res, next) => {
-    const targetUrl = req.query.url;
-
-    if (!targetUrl) {
-        return res.status(400).send('No URL provided');
-    }
-
-    // Parse the target URL
-    const parsedUrl = url.parse(targetUrl);
-
-    // Create a proxy middleware to forward the request
-    const proxy = createProxyMiddleware({
-        target: `${parsedUrl.protocol}//${parsedUrl.host}`,
-        changeOrigin: true,
-        pathRewrite: {
-            '^/proxy': parsedUrl.pathname + (parsedUrl.search || ''),
-        },
-        secure: false,
-        onProxyReq: (proxyReq, req, res) => {
-            // Modify the proxy request headers if needed
-        },
-        onProxyRes: (proxyRes, req, res) => {
-            // Modify the proxy response headers if needed
-        },
-    });
-
-    return proxy(req, res, next);
 });
 
 app.listen(process.env.PORT, () => console.log(`Listening at port ${process.env.PORT} ${process.env.MESSAGE}`));
